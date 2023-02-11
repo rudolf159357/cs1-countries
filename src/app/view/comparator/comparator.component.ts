f21e8577ad7fbbf5b8943928233671f17c8c9a84
rudolf <rudigard15@gmail.com>
2023-02-11 16:53:59 +0100

    comparator finished

--- src/app/view/comparator/comparator.component.ts
+++ src/app/view/comparator/comparator.component.ts
@@ -21,6 +21,11 @@ export class ComparatorComponent implements OnInit {
 
   error: boolean = false;
 
+  result: number = 0;
+  totalArea: number = 0;
+  population: number = 0;
+  cityPopulation: number = 0;
+
   constructor(private service: GetdataService) { }
 
   ngOnInit(): void {
@@ -49,7 +54,7 @@ export class ComparatorComponent implements OnInit {
     }});
   }
 
-  biggest(){
+  checkboxValidation(){
     // if all checkboxes are checked or blank, than show error
     if(this.checkbox.area == false && this.checkbox.population == false && this.checkbox.cityPopulation == false ||
       this.checkbox.area == true && this.checkbox.population == true && this.checkbox.cityPopulation == true ||
@@ -57,19 +62,151 @@ export class ComparatorComponent implements OnInit {
       || this.checkbox.population == true && this.checkbox.cityPopulation == true){
       this.error = true;
       console.log("error");
-      return;
-    }
-    // compare total area
-    if(this.checkbox.area == true){
-      console.log("area");
-
-    } else if (this.checkbox.population == true) {
-      console.log("population");
-    } else {
-      console.log("city population");
     }
   }
 
-  smallest(){}
+  biggestValue(value: number, biggest: number){
+
+    if(value > biggest && !(value <= 0)){
+      biggest = value;
+    } 
+    return biggest;
+
+  }
+
+  smallestValue(value: number, smallest: number){
+
+
+    if(value > smallest && !(value <= 0)){
+        smallest = value;
+      } 
+    else if(value < smallest && !(value <= 0)){
+      smallest = value;
+    } 
+    return smallest;
+
+  }
+
+  biggest(){
+
+    this.checkboxValidation();
+
+    if(this.error == false){
+        // compare total area
+      if(this.checkbox.area == true){
+        this.result = 0;
+
+        this.EuropeCountries.forEach(obj => {
+          this.totalArea = this.biggestValue(obj.totalArea, this.totalArea)
+        });
+
+        this.AmericaCountries.forEach(obj => {
+          this.totalArea = this.biggestValue(obj.totalArea, this.totalArea);
+        });
+
+        this.AsiaCountries.forEach(obj => {
+          this.totalArea = this.biggestValue(obj.totalArea, this.totalArea);
+        });
+
+        this.result = this.totalArea;
+
+      } else if (this.checkbox.population == true) {
+        this.result = 0;
+
+        this.EuropeCountries.forEach(obj => {
+          this.population = this.biggestValue(obj.population, this.population)
+        });
+
+        this.AmericaCountries.forEach(obj => {
+          this.population = this.biggestValue(obj.population, this.population);
+        });
+
+        this.AsiaCountries.forEach(obj => {
+          this.population = this.biggestValue(obj.population, this.population);
+        });
+
+        this.result = this.population;
+      } else if (this.checkbox.cityPopulation == true){
+        this.result = 0;
+
+        this.EuropeCountries.forEach(obj => {
+          this.cityPopulation = this.biggestValue(obj.cityPopulation, this.cityPopulation)
+        });
+
+        this.AmericaCountries.forEach(obj => {
+          this.cityPopulation = this.biggestValue(obj.cityPopulation, this.cityPopulation);
+        });
+
+        this.AsiaCountries.forEach(obj => {
+          this.cityPopulation = this.biggestValue(obj.cityPopulation, this.cityPopulation);
+        });
+
+        this.result = this.cityPopulation;
+      }
+    }
+
+    
+  }
+
+  smallest(){
+    this.checkboxValidation();
+
+    if(this.error == false){
+        // compare total area
+      if(this.checkbox.area == true){
+        this.result = 0;
+
+        this.EuropeCountries.forEach(obj => {
+          this.totalArea = this.smallestValue(obj.totalArea, this.totalArea)
+        });
+
+        this.AmericaCountries.forEach(obj => {
+          this.totalArea = this.smallestValue(obj.totalArea, this.totalArea);
+        });
+
+        this.AsiaCountries.forEach(obj => {
+          this.totalArea = this.smallestValue(obj.totalArea, this.totalArea);
+        });
+
+        this.result = this.totalArea;
+
+      } else if (this.checkbox.population == true) {
+        this.result = 0;
+
+        this.EuropeCountries.forEach(obj => {
+          this.population = this.smallestValue(obj.population, this.population)
+        });
+
+        this.AmericaCountries.forEach(obj => {
+          this.population = this.smallestValue(obj.population, this.population);
+        });
+
+        this.AsiaCountries.forEach(obj => {
+          this.population = this.smallestValue(obj.population, this.population);
+        });
+
+        this.result = this.population;
+      } else if (this.checkbox.cityPopulation == true){
+        this.result = 0;
+
+        this.EuropeCountries.forEach(obj => {
+          this.cityPopulation = this.smallestValue(obj.cityPopulation, this.cityPopulation)
+        });
+
+        this.AmericaCountries.forEach(obj => {
+          this.cityPopulation = this.smallestValue(obj.cityPopulation, this.cityPopulation);
+        });
+
+        this.AsiaCountries.forEach(obj => {
+          this.cityPopulation = this.smallestValue(obj.cityPopulation, this.cityPopulation);
+        });
+
+        this.result = this.cityPopulation;
+      }
+    }
+  }
+
+ 
+
 
 }
